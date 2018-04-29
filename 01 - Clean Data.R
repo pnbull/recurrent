93c6212941e08c9acfdc2587c673cbebb62b2500
jculhane <38705334+jculhane@users.noreply.github.com>
2018-04-28 18:20:25 -0700

    Add otforcep variable

--- 01 - Clean Data.R
+++ 01 - Clean Data.R
@@ -17,7 +17,7 @@ pt <- subset(pt_raw, select = c(exposed, mbgap_p2, momage_p1, momedu03_p1,
                                 hepatitc_p1, parity_p1, gestest_p1, spontdel_p1, labonset_p1, 
                                 rupmem_p1, indlab_p1, precipl_p1, longlab_p1, forcfail_p1, 
                                 vacuum_p1, vacfail_p1, stimlab_p1, csection_p1, dmethfin_p1, 
-                                fet_pres_p1, breech_p1, placprev_p1, 
+                                fet_pres_p1, breech_p1, placprev_p1, otforcep_p1,
                                 cmdiag1_p1:cmdiag25_p1, cbdiag1_p1:cbdiag25_p1, 
                                 cmproc1_p1:cmproc25_p1, cbproc1_p1:cbproc25_p1,
                                 momage_p2, momedu03_p2, momrace_p2, cigs_1st_p2, cigs_2nd_p2, 
@@ -28,12 +28,12 @@ pt <- subset(pt_raw, select = c(exposed, mbgap_p2, momage_p1, momedu03_p1,
                                 hepatitb_p2, hepatitc_p2, parity_p2, gestest_p2, spontdel_p2, 
                                 labonset_p2, rupmem_p2, indlab_p2, precipl_p2, longlab_p2, 
                                 forcfail_p2, vacuum_p2, vacfail_p2, stimlab_p2, csection_p2, 
-                                dmethfin_p2, fet_pres_p2, breech_p2, placprev_p2, 
+                                dmethfin_p2, fet_pres_p2, breech_p2, placprev_p2, otforcep_p2,
                                 cmdiag1_p2:cmdiag25_p2, cbdiag1_p2:cbdiag25_p2, 
                                 cmproc1_p2:cmproc25_p2, cbproc1_p2:cbproc25_p2))
 
 # quick look at all values for each variable (other than ICD9)
-for(i in c(1:45, 147:190)) {
+for(i in c(1:47, 148:192)) {
   print(names(pt[i]))
   print(table(pt[, i]))
 }
@@ -45,3 +45,19 @@ table(pt$gestest_p1, pt$exposed) # OK - all exposed have gestest_p1 < 37
 pt$gestest_p2[pt$gestest_p2>=99] <-NA
 pt$ipi = round(pt$mbgap_p2/7 - pt$gestest_p2, 0)
 table(pt$ipi) # Range from -10 to 489 weeks.  Sparse under 5 weeks.
+
+# investigate "labonset" variable
+table(pt$labonset_p1, pt$rupmem_p1)
+table(pt$labonset_p1, pt$precipl_p1)
+table(pt$labonset_p1, pt$longlab_p1)
+# "labonset" appears to be set to "Yes" if any of the other "onset of labor" box
+# vars are checked, and also for "none of the above" onsets of labor 
+# Klebanoff ignored this "none of the above" onset of labor checkbox.
+
+table(pt$csection_p1, pt$dmethfin_p1)
+table(pt$csection_p2, pt$dmethfin_p2)
+# csection type and final method of delivery are consistent.
+# use "dmethfin" for csection with trial of labor (provides more info than csection)
+
+table(pt$parity_p1, pt$parity_p2)
+# 80,842 2nd births with parity=1 or 999, of 86,554 1st births with parity= (93%)
